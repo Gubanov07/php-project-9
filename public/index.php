0bc3d0e67c5c35c2969835ff5dfe350ce3a0e6d9
Александр Губанов <Gubanov07>
2025-12-09 18:59:56 +0700

    fix index2

--- public/index.php
+++ public/index.php
@@ -121,9 +121,7 @@ $app->post('/urls', function ($request, $response) {
         $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
     }
     
-    return $response
-        ->withHeader('Location', $this->get('renderer')->getRouteParser()->urlFor('urls.show', ['id' => $urlId]))
-    ->withStatus(302);
+    return $response->withHeader('Location', '/urls/' . $urlId)->withStatus(302);
 });
 
 $app->post('/urls/{id}/checks', function ($request, $response, $args) {
